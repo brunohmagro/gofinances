a5988238ae44cdfd1d154965fc1987d0734d179a
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-06-01 16:48:52 -0300

    feat: add header with user info and signout button

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  SignOut,
+  Icon,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/48726014?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Bruno</UserName>
+            </User>
+          </UserInfo>
+
+          <SignOut onPress={() => console.log("Quero sair do app")}>
+            <Icon name="power" />
+          </SignOut>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,56 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.SafeAreaView`
+  height: ${RFValue(278)}px;
+  background: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View`
+  padding: 0 ${RFValue(24)}px;
+  margin-top: ${RFValue(8)}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: ${RFValue(10)}px;
+`;
+
+export const User = styled.View`
+  margin-left: ${RFValue(16)}px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const SignOut = styled.TouchableOpacity``;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
