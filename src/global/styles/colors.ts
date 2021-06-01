fca4c479806874424d2dd9dc7e9c411aa2a8f889
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-06-01 15:46:36 -0300

    feature: define global styles

---
+++ src/global/styles/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  primary: "#5636D3",
+  secondary: "#FF872C",
+  secondary_light: "#FF872C4D",
+  success: "#12A454",
+  success_light: "#12A45480",
+  attention: "#E83F5B",
+  attention_light: "#E83F5B80",
+  shape: "#fff",
+  title: "#363F5F",
+  text: "#969CB2",
+  background: "#F0F2F5",
+};
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 24px;
   font-weight: bold;
-  color: palevioletred;
+  color: ${({ theme }) => theme.colors.title};
 `;
