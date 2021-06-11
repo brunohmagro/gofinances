910d8e05757aeca95e3d7dd3bca426a605a5fb5f
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-06-11 08:37:29 -0300

    feat: add sum to cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -24,15 +24,29 @@ import {
   Title,
   TransactionsList,
 } from "./styles";
-import { date } from "yup/lib/locale";
 
 export interface DataListProps extends DataTransaction {
   id: string;
 }
 
+interface HighlightCardProps {
+  entries: {
+    amount: string;
+  };
+  expensives: {
+    amount: string;
+  };
+  total: {
+    amount: string;
+  };
+}
+
 export function Dashboard() {
   const keyTransactions = "@gofinances:transactions";
   const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [highlightData, setHighlightData] = useState<HighlightCardProps>(
+    {} as HighlightCardProps
+  );
 
   async function loadTransactions() {
     const currentTransactions = await Storage.getItem(keyTransactions);
@@ -41,8 +55,17 @@ export function Dashboard() {
       ? JSON.parse(currentTransactions)
       : ([] as DataListProps[]);
 
+    let entriesTotal = 0;
+    let expensive = 0;
+
     if (getTransactions) {
       const transactionsTrataive = getTransactions.map((transaction) => {
+        if (transaction.type === "positive") {
+          entriesTotal += Number(transaction.amount);
+        } else {
+          expensive += Number(transaction.amount);
+        }
+
         return {
           ...transaction,
           dateFormatted: new Date(transaction.date).toLocaleDateString(
@@ -58,6 +81,27 @@ export function Dashboard() {
 
       setTransactions(transactionsTrataive);
     }
+
+    setHighlightData({
+      entries: {
+        amount: Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(Number(entriesTotal)),
+      },
+      expensives: {
+        amount: Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(Number(expensive)),
+      },
+      total: {
+        amount: Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(Number(entriesTotal) - Number(expensive)),
+      },
+    });
 
     console.log(transactions);
   }
@@ -97,21 +141,21 @@ export function Dashboard() {
       <HighlightCards>
         <HighlightCard
           title="Entradas"
-          amount="R$ 17.000,00"
+          amount={highlightData.entries.amount}
           lastTransaction="Última entrada dia 13 de abril"
           type="up"
         />
 
         <HighlightCard
           title="Saídas"
-          amount="R$ 200,00"
+          amount={highlightData.expensives.amount}
           lastTransaction="Última saída dia 19 de abril"
           type="down"
         />
 
         <HighlightCard
           title="Total"
-          amount="R$ 16.800,00"
+          amount={highlightData.total.amount}
           lastTransaction="01 a 19 de abril"
           type="total"
         />
