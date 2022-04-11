60e32b0384ea6c9ca41dfe7548e9f37c20ca7933
Bruno Henrique Magro <brunohmagro@hotmail.com>
2022-04-11 18:42:57 -0300

    feat: add test suite

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,28 +3,30 @@ import { render } from '@testing-library/react-native'
 
 import { Profile } from '../../screens/Profile'
 
-test('should show username button placeholder correctly', () => {
-  const { getByPlaceholderText } = render(<Profile />)
+describe('Profile', () => {
+  it('should show username input placeholder correctly', () => {
+    const { getByPlaceholderText } = render(<Profile />)
+    
+    const inputName = getByPlaceholderText('Nome')
   
-  const inputName = getByPlaceholderText('Nome')
-
-  expect(inputName).toBeTruthy()
-})
-
-test('checks if user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />)
-
-  const inputName = getByTestId('input-name')
-  const inputSurname = getByTestId('input-surname')
-
-  expect(inputName.props.value).toEqual('Bruno')
-  expect(inputSurname.props.value).toEqual('Magro')
-})
-
-test('checks if title render correctly', () => {
-  const { getByTestId } = render(<Profile />)
-
-  const title = getByTestId('text-title')
-
-  expect(title.props.children).toContain('Perfil')
+    expect(inputName).toBeTruthy()
+  })
+  
+  it('should be load user data', () => {
+    const { getByTestId } = render(<Profile />)
+  
+    const inputName = getByTestId('input-name')
+    const inputSurname = getByTestId('input-surname')
+  
+    expect(inputName.props.value).toEqual('Bruno')
+    expect(inputSurname.props.value).toEqual('Magro')
+  })
+  
+  it('should exist title correctly', () => {
+    const { getByTestId } = render(<Profile />)
+  
+    const title = getByTestId('text-title')
+  
+    expect(title.props.children).toContain('Perfil')
+  })
 })
