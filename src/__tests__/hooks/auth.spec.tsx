8db2c0bf97a9ce3f1b39228d407f24c4d1a8c1e0
Bruno Henrique Magro <brunohmagro@hotmail.com>
2022-04-12 18:41:30 -0300

    feat: create new tests and fix mock with jest-mock

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,23 +1,11 @@
 import React from 'react'
 import { renderHook, act } from '@testing-library/react-hooks'
+import { mocked } from 'jest-mock'
+import { logInAsync } from 'expo-google-app-auth'
  
 import { AuthProvider, useAuth } from '../../hooks/auth'
 
-jest.mock('expo-google-app-auth', () => {
-  return {
-    logInAsync: () => {
-      return {
-        type: 'success',
-        user: {
-          id: '123-123-123',
-          email: 'any@mail.com',
-          name: 'AnyName',
-          photo: 'any_photo.png'
-        }
-      }
-    }
-  }
-})
+jest.mock('expo-google-app-auth')
 
 const Providers: React.FC = ({ children }) => (
   <AuthProvider>
@@ -27,6 +15,17 @@ const Providers: React.FC = ({ children }) => (
 
 describe('Auth Hook', () => {
   it('should be able to sign with Google account existing', async () => {
+    const googleMocked = mocked(logInAsync as any)
+    googleMocked.mockReturnValue({
+      type: 'success',
+      user: {
+        id: '123-123-123',
+        email: 'any@mail.com',
+        name: 'AnyName',
+        photo: 'any_photo.png'
+      }
+    })
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: Providers
     })
@@ -34,5 +33,20 @@ describe('Auth Hook', () => {
     await act(() => result.current.signInWithGoogle())
 
     expect(result.current.user.email).toBe('any@mail.com')
+  })
+
+  it('should not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(logInAsync as any)
+    googleMocked.mockReturnValue({
+      type: 'cancel',
+    })
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: Providers
+    })
+
+    await act(() => result.current.signInWithGoogle())
+
+    expect(result.current.user).not.toHaveProperty('id')
   })
 })
